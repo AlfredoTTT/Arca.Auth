a6afb2cb700d816d3febeb4366fde58c04eb69e7
Tovar <alfredo.tovar@externo.infra.com.mx>
2026-07-16 10:55:26 -0600

    testing

--- Auth.Api/Program.cs
+++ Auth.Api/Program.cs
@@ -11,6 +11,18 @@ builder.Services.AddJwtAuthentication(builder.Configuration);
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
 var app = builder.Build();
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+if (string.IsNullOrEmpty(connectionString))
+{
+    logger.LogCritical("¡ERROR CRÍTICO! La Connection String 'DefaultConnection' está vacía.");
+}
+else
+{
+    var serverName = connectionString.Split(';').FirstOrDefault(x => x.StartsWith("Server"));
+    logger.LogInformation("Conexión detectada. Servidor: {ServerName}", serverName);
+}
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseHttpsRedirection();
--- Auth.Application/Features/Users/Commands/RegisterUserCommand.cs
+++ Auth.Application/Features/Users/Commands/RegisterUserCommand.cs
@@ -1,4 +1,4 @@
 using MediatR;
 
 namespace Auth.Application.Features.Users.Commands;
-public record RegisterUserCommand(string Email, string Password, Guid RoleId): IRequest;
+public record RegisterUserCommand(string Email, string Password, Guid RoleId): IRequest<Unit>;
